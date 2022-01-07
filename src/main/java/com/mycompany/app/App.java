c3338fff2b68c930873b8bda46aa1e779c116c7e
damian-curiel <89002083+damian-curiel@users.noreply.github.com>
2022-01-07 17:20:32 -0600

    app update

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+//Poderosisimo Hola Mundo
+
+public class App
+{
+
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
